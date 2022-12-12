24b00c4ae9e2ead3623be3013bb57ba41b17c01a
songjh <songjh@DESKTOP-QITPULD>
2022-12-12 14:29:06 +0900

    project 최초커밋

---
+++ myPrj/src/test/java/com/jh/MyPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
